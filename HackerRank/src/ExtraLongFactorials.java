b9bb91168b6ac76d82163541f99b46091662c4aa
Elmerrrrrr <volgers80@gmail.com>
2021-04-15 20:37:22 +0200

    update

--- HackerRank/src/ExtraLongFactorials.java
+++ HackerRank/src/ExtraLongFactorials.java
@@ -10,7 +10,6 @@ public class ExtraLongFactorials {
         for (int i=(n-1); i>0; i--) {
             sum=sum.multiply(BigInteger.valueOf(i));
         }
-        System.out.println(sum);
     return sum;
     }
 }
